b0a7990a9b73e81f3a7faa1dab2f26e6f44518b7
Courtney White <white.courtney.lynn@gmail.com>
2025-02-15 14:10:46 -0600

    users are successfully created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
   end
-  
+
   def after_sign_in_path_for(resource)
     root_path
   end
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,27 @@
 class AuthenticationController < ApplicationController
   # Optional: Redirect if already logged in
   before_action :redirect_if_authenticated
-
+  helper Devise::Controllers::Helpers
+  helper_method :resource_name, :resource_class, :devise_mapping
   def index
-    # This action will render the custom authentication page
-    # It can display both login and registration forms
-    @user = User.new
+    # This is necessary to make resource available
+    @resource = resource_class.new
   end
 
   private
+
+  # These methods are required to make Devise helpers work
+  def resource_name
+    :user
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 
   def redirect_if_authenticated
     if user_signed_in?
