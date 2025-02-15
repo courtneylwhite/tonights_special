8279842dea0193bed014e3ea23fb7e4ad0f36c9d
Courtney White <white.courtney.lynn@gmail.com>
2025-02-14 18:44:57 -0600

    add tests

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    roots: ['<rootDir>/jest'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/app/javascript/$1'
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest/setupTests.js'],
+    transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest'
+    },
+    transformIgnorePatterns: ['node_modules/(?!@babel/runtime)']
+};
