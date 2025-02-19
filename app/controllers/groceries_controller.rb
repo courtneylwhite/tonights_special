60a135a64e67808344534e9cbcadc20a3c0c7945
Courtney White <white.courtney.lynn@gmail.com>
2025-02-18 22:50:04 -0600

    fix unit display on component

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -2,13 +2,13 @@ class GroceriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groceries = current_user.groceries.includes(:grocery_section)  # Change to instance variable
+    @groceries = current_user.groceries.includes(:grocery_section, :unit)  # Change to instance variable
     @grouped_groceries = @groceries.map do |grocery|
       {
         id: grocery.id,
         name: grocery.name,
         quantity: grocery.quantity,
-        unit_id: grocery.unit_id,
+        unit: grocery.unit.abbreviation,
         emoji: grocery.emoji,
         category: grocery.grocery_section.name
       }
