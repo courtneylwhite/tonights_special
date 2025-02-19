c4ac302c2eda2bdeb425519e4b2280d81595cdab
Courtney White <white.courtney.lynn@gmail.com>
2025-02-19 17:03:53 -0600

    extract components

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,7 +1,6 @@
 // javascript/components/index.js
 export { default as AnimatedTitle } from './AnimatedTitle'
-export { default as Groceries } from './Groceries'
+export { default as Pantry } from './Pantry'
 export { default as GroceryItem } from './GroceryItem'
-export { default as SectionModal } from './SectionModal'
-export { default as ItemModal } from './ItemModal'
+
 // Add more components here
